4f37a87699f9a586ab5dcb76f0790b0ae42aa253
sunny <sunnyjujjuvarapu@gmailcom>
2021-09-03 02:15:24 +0530

    updated

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express=require('express')
 const app=express()
 const request=require('request') 
 
-//Middlewares  ejs is atemplating language
+//Middlewares  ejs is a templating language
 app.set("view engine","ejs")
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => {  //response and request //request is sending some id and getting access
+    
  
   res.render("home") // to display html page we use render
-  })
   
+})
 
 app.get("/student", (req, res) => {
     res.send('kill')
@@ -24,7 +25,7 @@ app.get("/student/:rollno", (req, res) => {
 app.get("/result", (req, res) => {
     console.log(req.query)
     // res.send(`you searched for ${req.query.Moviename}`)
-   const url=`http://www.omdbapi.com/?apikey=cfd672ef&s=${req.query.Moviename}`
+   const url=`http://www.omdbapi.com/?apikey=cfd672ef&s=${req.query.Moviename}` 
    request(url,function(error,response,body){
 
        if(!error && response.statusCode===200)
@@ -38,41 +39,36 @@ app.get("/result", (req, res) => {
        }
     })
 })
-/*
-app.get("/result/:id", (req, res) => {
-    const url=`http://www.omdbapi.com/?apikey=cfd672ef&i=${req.params.id}`
-    request(url,function(error,response,body){
- 
-        if(!error && response.statusCode===200)
-        {
-            const data=JSON.parse(body)
-           // res.send(data)
-           //res.render('result',{moviesDump:data})
-           res.render('detail',{data:data})
-        }
-        else{
-         res.send('something went wrong')
-        }
-     })
-})
-*/
-app.get('/result/:id', (req, res)=>{
+
+/*app.get('/result/:id', (req, res)=>{ 
     const url=`http://www.omdbapi.com/?apikey=cfd672ef&i=${req.params.id}`
     request(url, function (error, response, body){
         if(!error && response.statusCode===200){
-            const data=JSON.parse(body)
+            const data=JSON.parse(body) 
             res.render('detail', {data: data})
         }else{
             res.send('Something went wrong')
         }
     })
 })
-
+*/
+app.get('/result/:id/info', (req, res)=>{ 
+    const url=`http://www.omdbapi.com/?apikey=cfd672ef&i=${req.params.id}`
+    request(url, function (error, response, body){
+        if(!error && response.statusCode===200){
+            const data=JSON.parse(body) 
+            res.render('info', {movie: data})
+        }else{
+            res.send('Something went wrong')
+        }
+    })
+})
 
 app.get('*',(req,res)=>{
     res.send('Invalid Route')
 })
 
 app.listen(5000,()=>{
+
     console.log('server has started')
 })
